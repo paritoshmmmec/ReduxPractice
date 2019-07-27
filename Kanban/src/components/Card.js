88c148b4228553ad87039f113dcc31afd2fe64d7
paritosh baghel <paritoshmmmec@gmail.com>
2019-07-27 07:02:13 -0400

    Styling

--- Kanban/src/components/Card.js
+++ Kanban/src/components/Card.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CheckList from './CheckList';
 
-const Card = ({card}) => {
-    return (
-        <div className="card" key ={card.id}>
-            <div className="card__title">{card.title}</div>
-            <div className="card__details">
-                {card.description}
-                <CheckList cardId={card.id} tasks={card.tasks} />
-            </div>
-        </div>
-    );
+class Card extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            showDetails: false
+        }
+    }
+
+    handleDetails = () => {
+        this.setState({ showDetails: !this.state.showDetails });
+    }
+
+    render() {
+        return (
+            <div className="card" key={this.props.card.id}>
+                <div className="card__title" onClick={this.handleDetails}>
+                    {this.props.card.title}
+                </div>
+                <div className="card__details">
+                    {this.props.card.description}
+                    {this.state.showDetails && <CheckList cardId={this.props.card.id} tasks={this.props.card.tasks} />}
+                </div>
+            </div >
+        );
+    }
 }
 
 export default Card;
